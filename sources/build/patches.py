834630761a58405860178e54199883655ca4f253
VanillaDaFur <you@example.com>
2025-04-30 11:17:15 +0300

    fixed build error and removed useless patches

--- sources/build/patches.py
+++ sources/build/patches.py
@@ -8,15 +8,14 @@ def apply_colloid_patches(colloid_dir, patch_dir):
     colloid_dir = Path(colloid_dir).relative_to(os.getcwd())
     if os.path.isfile(colloid_dir / ".patched"):
         logger.info(
-            f'Patches seem to be applied, remove "{colloid_dir}/.patched" to force application (this may fail)'
+            f'Patches seem to be applied, remove "{
+                colloid_dir}/.patched" to force application (this may fail)'
         )
         return
 
     logger.info("Applying patches...")
     # Change into colloid
     for patch in [
-        "plank-dark.patch",
-        "plank-light.patch",
         "sass-palette-frappe.patch",
         "sass-palette-mocha.patch",
         "sass-palette-latte.patch",
